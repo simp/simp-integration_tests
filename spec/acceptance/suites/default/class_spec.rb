0f68ad09c5bef7f6e8c2306ae9d61eebb3820eb4
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-09-06 16:36:37 +0000

    Add beaker smoke test

--- spec/acceptance/suites/default/class_spec.rb
+++ spec/acceptance/suites/default/class_spec.rb
@@ -5,7 +5,9 @@ test_name 'integration_tests class'
 describe 'integration_tests class' do
   let(:manifest) {
     <<-EOS
-      file{ '/root/smoke_test.txt': content => 'Beaker wrote this file!' }
+      file{ '/root/.beaker-suites.smoke_test.file':
+        content => 'Beaker wrote this file!'
+      }
     EOS
   }
 
@@ -16,6 +18,11 @@ describe 'integration_tests class' do
 
     it 'should be idempotent' do
       apply_manifest(manifest, :catch_changes => true)
+    end
+
+    it 'should create the smoke test file' do
+      puppetserver = find_only_one('puppetserver')
+      on puppetserver, 'test -f /root/.beaker-suites.smoke_test.file'
     end
   end
 end
