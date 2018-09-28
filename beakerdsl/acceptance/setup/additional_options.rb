2b9eaa138c5cc5573369c3331df32b555fee2b09
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-09-28 19:18:25 +0000

    Remove beakerdsl examples

--- beakerdsl/acceptance/setup/additional_options.rb
+++
@@ -1,9 +0,0 @@
-puts '====================================================== acceptance/setup/additional_options.rb'
-{
-  :epel_url => 'http://192.168.103.7',
-  :add_epel_extras => false,
-  :fail_mode => 'fast',
-  :log_level => 'debug',
-  :log_level => 'trace',
-
-}
