ccfaa4b229b59d898941278f98eeafe42715a3b7
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-09-28 19:17:58 +0000

    Remove module files (part two)

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -10,25 +10,6 @@ include Simp::BeakerHelpers
 # Note that ISO integration tests don't need to run install_puppet
 
 RSpec.configure do |c|
-  # ensure that environment OS is ready on each host
-  # fix_errata_on hosts
-
   # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    begin
-      # Install modules and dependencies from spec/fixtures/modules
-      # copy_fixture_modules_to(hosts)
-    rescue StandardError, ScriptError => e
-      # rubocop:disable Style/GuardClause, Style/Semicolon, Lint/Debugger
-      if ENV['PRY']
-        require 'pry'; binding.pry
-      else
-        raise e
-      end
-      # rubocop:enable Style/GuardClause, Style/Semicolon, Lint/Debugger
-    end
-  end
 end
